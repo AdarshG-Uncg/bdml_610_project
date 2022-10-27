865f1b16ed2d426bb4aebd8b6de18bbeb721a958
AdarshG_Uncg <a_gadari@uncg.edu>
2022-10-27 14:36:34 -0400

    removed index in dataset

---
+++ exploration ipynb files/baseline_modelling.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import os
+
+
+parent_path = r'C:\Users\a_gadari\OneDrive - UNCG\Documents\bdml'
+os.chdir(parent_path)
+
+
+df = pd.read_csv('./dataframes/8xblocks_labelled_dataset.csv')
+
+
