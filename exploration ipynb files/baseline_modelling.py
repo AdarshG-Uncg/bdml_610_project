42e4c8f286483a1b4c0cdd121747b6b1210d3396
AdarshG_Uncg <a_gadari@uncg.edu>
2022-10-28 17:05:51 -0400

    hanshu bday commit

--- exploration ipynb files/baseline_modelling.py
+++ exploration ipynb files/baseline_modelling.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 import tensorflow as tf
 from tensorflow.keras import layers, models
-
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
@@ -18,26 +18,33 @@ data = np.array(df.drop(columns='labels'))
 
 X_train, X_test, y_train, y_test = train_test_split(data, labels, stratify = labels, test_size=0.33, random_state=21)
 
-'''
 x_label = ['Total','train','test']
 y1 = np.array([sum(labels)/df.shape[0],sum(y_train)/X_train.shape[0],sum(y_test)/X_test.shape[0]])
 y2 = np.array([100]*3) - y1
 print(y1, y2)
 X_axis = np.arange(len(x_label))
-
+'''
 plt.bar(X_axis - 0.2, y2,0.4, label = 'label 0')
 plt.bar(X_axis + 0.2, y1,0.4, label = 'label 1')
 plt.show()
-print(sum(y_train),sum(y_test))
 '''
 
 model = models.Sequential()
-model.add(layers.Conv2D(16, (3, 3), activation='relu', input_shape=(8, 8, 1)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(8, (3, 3), activation='relu'))
+model.add(layers.Conv2D(4, (3, 3), input_shape=(8, 8, 1)))
+#model.add(layers.MaxPooling2D((2, 2)))
+#model.add(layers.Conv2D(8, (3, 3), activation='relu'))
 model.add(layers.Flatten())
 model.add(layers.Dense(1,activation='sigmoid'))
 model.compile(optimizer='adam', loss=tf.keras.losses.BinaryCrossentropy(from_logits=False),metrics=['Recall'])
 
 print(model.summary())
+
+train_images = np.array([np.array(i).reshape(8,8) for i in list(X_train)])
+test_images = np.array([np.array(i).reshape(8,8) for i in list(X_test)])
+
+history = model.fit(train_images, np.array(y_train), epochs=10, validation_data=(test_images, np.array(y_test)))
+
+pred = model.predict(test_images)
+pred = [1 if x>0.5 else 0 for x in pred]
+print(confusion_matrix(y_test,pred))
 
